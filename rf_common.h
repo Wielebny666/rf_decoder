b4fa0e816470134b672e2ccc7b6d8688f24de6b7
Wielebny666 <31576684+Wielebny666@users.noreply.github.com>
2020-10-24 17:09:12 +0200

    Zmiana nazwy

--- rf_common.h
+++ rf_common.h
@@ -26,34 +26,39 @@ extern "C"
  *      TYPEDEFS
  **********************/
 typedef void *rf_dev_t;
-typedef struct rf_coder_s rf_coder_t;
-typedef struct rf_decoder_s rf_decoder_t;
+typedef struct rf_scrambler_s rf_scrambler_t;
+typedef struct rf_descrambler_s rf_descrambler_t;
 
-struct rf_coder_s
+struct rf_scrambler_s
 {
-
+    esp_err_t (*make_preamble)(rf_scrambler_t *scrambler);
+    esp_err_t (*make_sync)(rf_scrambler_t *scrambler);
+    esp_err_t (*make_payload)(rf_scrambler_t *scrambler);
+    esp_err_t (*make_crc)(rf_scrambler_t *scrambler);
+    esp_err_t (*get_result)(rf_scrambler_t*scrambler, void *result, uint32_t *length);
+    esp_err_t (*del)(rf_scrambler_t *scrambler);
 };
 
-struct rf_decoder_s
+struct rf_descrambler_s
 {
-	esp_err_t (*input)(rf_decoder_t *parser, void *raw_data, uint32_t length);
-    esp_err_t (*get_scan_code)(rf_decoder_t *parser, uint32_t *address, uint32_t *command, bool *repeat);
-    esp_err_t (*del)(rf_decoder_t *parser);
+    esp_err_t (*input)(rf_descrambler_t *descrambler, void *raw_data, uint32_t length);
+    esp_err_t (*get_scan_code)(rf_descrambler_t *descrambler);
+    esp_err_t (*del)(rf_descrambler_t *decoder);
 };
 
 typedef struct
 {
-	rf_dev_t dev_hdl;
-	uint32_t flags;
-	uint32_t buffer_size;
-}rf_coder_config_t;
+    rf_dev_t dev_hdl;
+    uint32_t flags;
+    uint32_t buffer_size;
+}rf_scrambler_config_t;
 
 typedef struct
 {
-	rf_dev_t dev_hdl;
-	uint32_t flags;
-	uint32_t margin_us;
-}rf_decoder_config_t;
+    rf_dev_t dev_hdl;
+    uint32_t flags;
+    uint32_t margin_us;
+}rf_descrambler_config_t;
 
 /**********************
  * GLOBAL PROTOTYPES
@@ -64,7 +69,7 @@ typedef struct
  **********************/
 
 #ifdef __cplusplus
-} /* extern "C" */
+    } /* extern "C" */
 #endif
 
 #endif /* COMPONENTS_RF_DECODER_RF_COMMON_H_ */
